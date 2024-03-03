a2097524398de270301740eb8632031a8ea4a119
Leandro Silva <leandrosj7@gmail.com>
2024-03-03 23:53:39 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-let nome = "Leandro Silva"
-console.log("nome")
+//Heroi
+let heroi = "Flash"
+let XPheroi = 10005
+let nivelHeroi = "Radiante"
+
+console.log("O Herói de nome " + heroi + " está no nível " + nivelHeroi)
